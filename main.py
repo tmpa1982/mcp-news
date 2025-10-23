2892ac98920294d37cbbee79e99bc9b8c0fdc52e
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-10-23 09:52:28 +0700

    Skeleton MCP server

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from mcp-news!")
+from mcp.server.fastmcp import FastMCP
 
+def main():
+    mcp = FastMCP("news")
+    mcp.run(transport='stdio')
 
 if __name__ == "__main__":
     main()
