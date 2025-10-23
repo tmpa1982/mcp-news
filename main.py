8e6d340f4bccec02b503b984d834647cbcdeae0b
Tran Minh Phuong Anh <tran.minh.phuong.anh@gmail.com>
2025-10-23 09:53:45 +0700

    Add logging

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import logging
+
 from mcp.server.fastmcp import FastMCP
 
 def main():
     mcp = FastMCP("news")
+    logging.info("MCP server starting.")
     mcp.run(transport='stdio')
 
 if __name__ == "__main__":
